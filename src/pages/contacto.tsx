2b2c4f42cbe1dd545bd4e11a9d2a09ef37a9410a
DevSlashRichie <me@slashrichie.dev>
2023-05-10 18:53:55 -0600

    fix: responsive contacto

--- src/pages/contacto.tsx
+++ src/pages/contacto.tsx
@@ -2,6 +2,17 @@ import { HTMLInputTypeAttribute, useState } from "react";
 import { faFeather, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+function FieldDiv(
+  { children }: 
+  { children: React.ReactNode }
+) {
+  return (
+    <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-5">
+      { children }
+    </div>
+  );
+}
+
 function MultiplePicker({
   display,
   values
@@ -10,12 +21,10 @@ function MultiplePicker({
     values: string[],
 }) {
 
-  const [ picked, setPicked, ] = useState<Array<number>>([]);
-
   return (
-    <div className="flex items-center gap-5">
-      <span className="font-bold basis-40">{display}</span>
-      <div className="flex flex-col justify-center">
+    <FieldDiv>
+      <span className="font-bold sm:basis-40">{display}</span>
+      <div className="flex flex-col justify-center flex-1 w-full">
         {
           values.map((it, key) => {
             return <div key={`${it}-${key}`}>
@@ -25,7 +34,7 @@ function MultiplePicker({
           })
         }
       </div>
-    </div>
+    </FieldDiv>
   );
 }
 
@@ -37,9 +46,9 @@ function SinglePick({
     options: Array<string>
 }) {
   return (
-    <div className="flex items-center gap-5">
-      <span className="font-bold basis-40">{display}</span>
-      <select className="bg-timberwolf rounded-lg p-2 outline-transparent focus:outline-none flex-1">
+    <FieldDiv>
+      <span className="font-bold sm:basis-40">{display}</span>
+      <select className="bg-timberwolf rounded-lg p-2 outline-transparent focus:outline-none flex-1 w-full">
         {
           options.map((it, key) => {
             return <option key={`${it}-${key}`}>
@@ -48,7 +57,7 @@ function SinglePick({
           })
         }
       </select>
-    </div>
+    </FieldDiv>
   );
 }
 
@@ -63,23 +72,23 @@ function Field({
     placeholder: string,
   }) {
   return (
-    <div className="flex items-center gap-5">
-      <span className="font-bold basis-40">{display}</span>
+    <FieldDiv>
+      <span className="font-bold sm:basis-40">{display}</span>
       {
         type === "textfield"
           ? <textarea
-            className="bg-timberwolf rounded-lg p-2 outline-transparent focus:outline-none flex-1"
+            className="bg-timberwolf rounded-lg p-2 outline-transparent focus:outline-none flex-1 w-full"
             placeholder={placeholder}
           >
 
           </textarea>
           : <input 
-            className="bg-timberwolf rounded-lg p-2 outline-transparent focus:outline-none flex-1"
+            className="bg-timberwolf rounded-lg p-2 outline-transparent focus:outline-none flex-1 w-full"
             type={type}
             placeholder={placeholder} 
           />
       }
-    </div>
+    </FieldDiv>
   );
 }
 
@@ -97,9 +106,9 @@ function LiWithIcon({
 
 export default function  Contacto() {
   return (
-    <section className="py-10 lg:py-20 w-full flex justify-center">
-      <div className="flex lg:flex-row flex-col w-[1080px]">
-        <div className="w-full lg:w-1/2 flex flex-col gap-5 p-5">
+    <section className="md:py-20 w-full flex justify-center">
+      <div className="flex md:flex-row flex-col w-[1080px]">
+        <div className="w-full md:w-1/2 flex flex-col gap-5 p-5">
           <h1 className="text-6xl font-bold">Habla con un <b className="text-bittersweet">experto</b></h1>
           <b>Nuestro equipo puede ayudarte:</b>
           <ul className="list-none ml-6 list-inside">
@@ -110,8 +119,8 @@ export default function  Contacto() {
             <LiWithIcon icon={faFeather}>Servicio excepcional garantizado.</LiWithIcon>
           </ul>
         </div>
-        <div className="flex justify-between bg-white shadow-xl p-8 w-full lg:w-1/2 rounded-lg">
-          <div className="flex flex-col w-full gap-2">
+        <div className="flex justify-between bg-white shadow-xl p-8 w-full md:w-1/2 rounded-lg">
+          <div className="flex flex-col w-full gap-5 md:gap-2">
             <Field display="Nombre" type="text" placeholder="Daniel" />
             <Field display="Apellido" type="text" placeholder="Mendez" />
             <Field display="Correo" type="email" placeholder="ejemplo@ejemplo.com" />
@@ -119,7 +128,7 @@ export default function  Contacto() {
             <Field display="Necesitas algo mas?" type="textfield" placeholder="Hola, me gustaria..." />
             <SinglePick display="Tipo de operacion" options={["Venta", "Renta", "Compra"]} />
             <MultiplePicker display="Tipo de propiedad" values={["Casa", "Departamento", "Terreno"]} />
-            <button className="bg-skobeloff text-white rounded-lg p-2 w-32 self-end hover:bg-bittersweet transition">Enviar</button>
+            <button className="bg-skobeloff text-white rounded-lg mt-2 lg:mt-0 p-2 w-full lg:w-32 self-end hover:bg-bittersweet transition">Enviar</button>
           </div>
         </div>
       </div>
