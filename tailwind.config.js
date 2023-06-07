c9a498febfdc49d986ac5fb94229238cd0cb3160
DevSlashRichie <me@slashrichie.dev>
2023-06-07 06:23:16 -0600

    f

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
     extend: {
       colors: {
         // timberwolf: "#edf2f1",
-        timberwolf: "#edf2f1",
+        timberwolf: "#FFFAF1",
         wenge: "#524948",
-        bittersweet: "#BA8F95",
+        bittersweet: "#ffa500",
         //skobeloff: "#0b7a75",
-        skobeloff: "#2e4057",
+        skobeloff: "#8492a6",
         "dark-spring-green": "#2c6e49",
       },
       fontFamily: {
