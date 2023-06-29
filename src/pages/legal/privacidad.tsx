2c4d19cf856fe45b62dc9618e65927f4c9cdb91a
Apocalix <lololertrololer@gmail.com>
2023-06-28 20:10:22 -0600

    feat: added legal pages templates

---
+++ src/pages/legal/privacidad.tsx
@@ -0,0 +1,70 @@
+export default function Privacidad() {
+    return (
+            <main>
+            <section>
+                <div className="flex flex-col text-center gap-8 justify-center items-center bg-skobeloff text-white py-32">
+                    <p className="text-3xl lg:text-5xl font-bold w-full md:max-w-[700px] ">
+                        Política de privacidad
+                    </p>
+                    <p>
+                        La privacidad de nuestros usuarios es importante para nosotros.
+                    </p>
+                </div>
+            </section>
+            <section className="w-full flex">
+                <div className="flex flex-col w-full h-fit px-[512px] py-32">
+                    <div className="flex flex-col justify-start gap-8 text-start self-start sm:pt-10">
+                        <p className="text-xl lg:text-2xl w-full font-bold">
+                            Ultima vez actualizado: Junio 28, 2023
+                        </p>
+                        <p className="text-2xl lg:text-3xl font-bold w-full md:max-w-[700px] pt-8">
+                            Politicas de privacidad
+                        </p>
+                        <p className="text-xl lg:text-2xl w-full">
+                            Esta Política de privacidad describe nuestras políticas y procedimientos sobre la recopilación, uso y divulgación de su información cuando utiliza el Servicio y le informa sobre sus derechos de privacidad y cómo la ley lo protege.
+                            <br/><br/>
+                            Utilizamos sus datos personales para proporcionar y mejorar el Servicio. Al utilizar el Servicio, usted acepta la recopilación y el uso de información de acuerdo con esta Política de privacidad. Esta Política de privacidad se ha creado con la ayuda del generador de políticas de privacidad de TermsFeed.
+                            <br/><br/>
+                        </p>
+                        <p className="text-2xl lg:text-3xl font-bold w-full md:max-w-[700px] pt-8">
+                            Interpretación y definiciones
+                        </p>
+
+                        <p className="text-xl lg:text-2xl w-full font-bold">
+                            Interpretación
+                        </p>
+                        <p className="text-xl lg:text-2xl w-full">
+                            Las palabras cuya letra inicial está en mayúscula tienen significados definidos en las siguientes condiciones. Las siguientes definiciones tendrán el mismo significado independientemente de si aparecen en singular o en plural.
+                            <br/><br/>
+                        </p>
+                        <p className="text-xl lg:text-2xl w-full font-bold">
+                            Definiciones
+                        </p>
+                        <p className="text-xl lg:text-2xl w-full">
+                            A los efectos de esta Política de privacidad:
+                            <br/><br/>
+                        </p>
+
+                        <ul className="list-disc list-inside">
+                                <li className="text-xl lg:text-2xl w-full">
+                                    <span className="font-bold">Cuenta</span> significa una cuenta única creada para que usted acceda a nuestro Servicio o partes de nuestro Servicio.
+                                    <br/>
+                                    <br/>
+                                </li>
+                                <li className="text-xl lg:text-2xl w-full">
+                                    <span className="font-bold">Afiliado</span> significa una entidad que controla, es controlada por o está bajo control común con una parte, donde "control" significa propiedad del 50% o más de las acciones, participación patrimonial u otros valores con derecho a voto para la elección de directores u otra autoridad de gestión.
+                                    <br/>
+                                    <br/>
+                                </li>
+                                <li className="text-xl lg:text-2xl w-full">
+                                    <span className="font-bold">Compañía</span>  (referida como "la Compañía", "Nosotros", "Nuestro" en este Acuerdo) se refiere a asd, 1234 Main Street, Anytown, CA 12345.
+                                    <br/>
+                                    <br/>
+                                </li>
+                            </ul>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
