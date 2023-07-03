4c3ac425841615360645a671849e7195bf1abc0c
Apocalix <lololertrololer@gmail.com>
2023-07-02 22:38:15 -0600

    fix: responsive design
    fixed problem with titles and paddings in /legal and in /blog

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -13,16 +13,16 @@ export default function Blog({ posts }: InferGetStaticPropsType<typeof getStatic
     return (
         <main>
             <section className="w-full flex">
-                <div className="w-full flex flex-col text-center gap-8 justify-center items-center bg-timberwolf p-48 max-[400px]:p-10">
-                    <h3 className="text-lg lg:text-3xl max-[400px]:text-sm">
+                <div className="w-full flex flex-col gap-8 p-48 max-[400px]:p-10 min-[400px]:px-10 min-[400px]:py-16 lg:p-48 text-white bg-timberwolf py-24 px-5 md:p-24 justify-center items-start bg-[url('/irapuato.jpg')] bg-no-repeat bg-cover bg-center">
+                    <h3 className="text-lg sm:text-xl md:text-2xl xl:text-3xl max-[400px]:text-sm">
                         Explora nuestro blog
                     </h3>
-                    <p className="w-full text-3xl max-[400px]:text-xl min-[400px]:text-5xl font-bold">
+                    <p className="text-xl sm:text-2xl md:text-3xl xl:text-5xl max-[400px]:text-xl font-bold w-3/5 sm:w-1/2">
                         Enterate de nuestras ultimas novdedades, noticias y actualizaciones en una sola pagina
                     </p>
                 </div>
             </section>
-            <section className='grid grid-cols-2 px-64 max-[400px]:p-10 py-16 gap-8'>
+            <section className='grid grid-cols-1 md:grid-cols-2 px-48 2xl:px-40 xl:px-32 md:px-24 min-[400px]:px-10 min-[400px]:py-16 max-[400px]:p-10 xl:py-32 gap-8'>
                 {posts.map((post) => (
                     <div className='rounded-lg border border-black hover:bg-slate-200 bg-white hover:scale-[1.01] transition-all' key={post.id}>
                         <Link href={`/posts/${post.id}`}>
